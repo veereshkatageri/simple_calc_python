619e2d2b42faf5cdcea19dedbc8c7526f6118009
veeresh <veeresh.mcis@gmail.com>
2021-01-16 10:32:09 +0100

    Add __init__.py

---
+++ simple_calculator/__init__.py
@@ -0,0 +1,16 @@
+# !/usr/bin/python
+# coding=UTF-8
+# Written by Veeresh Katageri <veeresh.mcisi@gmail.com>, March 2020
+
+"""This script is used to import the scripts as submodules
+
+- author: Veeresh Katageri
+- status: alpha
+- e-mail: veeresh.mcisi@gmail.com
+"""
+
+
+try:
+    from simple_calculator import *
+except:
+    print("Unable to import")
