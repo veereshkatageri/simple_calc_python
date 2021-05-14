68460a104e20c304eb5706d0388a08623dc4191b
Katageri, Veeresh <veeresh.mcis@gmail.com>
2021-05-14 11:31:54 +0200

    Fix typos

--- simple_calculator/subtraction.py
+++ simple_calculator/subtraction.py
@@ -2,7 +2,7 @@
 # coding=UTF-8
 # Written by Veeresh Katageri <veeresh.mcisi@gmail.com>, March 2020
 
-"""This script is used to perform Substraction
+"""This script is used to perform Subtraction
 
 - author: Veeresh Katageri
 - status: alpha
@@ -12,12 +12,12 @@
 import os
 import argparse
 
-class Substraction:
+class Subtraction:
 
-    """Class Substraction is used to perform Substraction operation"""
+    """Class Subtraction is used to perform Subtraction operation"""
 
     def __init__(self, number_1, number_2):
-        """The init function of the class Substraction
+        """The init function of the class Subtraction
 
         :param number_1: is the number_1
         :type number_2: str
@@ -29,10 +29,10 @@ class Substraction:
         self.number_2 = int(number_2) if number_2.isnumeric() else float(number_2)
 
     def run(self):
-        """ The main function for Class Substraction."""
+        """ The main function for Class Subtraction."""
 
         result = self.number_1 - self.number_2
-        print("Substraction of numbers", result)
+        print("Subtraction of numbers", result)
         return result
 
 
@@ -42,16 +42,16 @@ def main(number_1="", number_2=""):
         2. it can be called standalone from command line.
     """
 
-    parser = argparse.ArgumentParser(description="Substraction")
+    parser = argparse.ArgumentParser(description="Subtraction")
     parser.add_argument('-n1', '--number_1', type=str, help='A valid 1st number')
     parser.add_argument('-n2', '--number_2', type=str, help='A valid 2nd number')
 
-    print("Substraction")
+    print("Subtraction")
     if not number_1.strip() and not number_2.strip():
         args = parser.parse_args()
         if 1:
             print("Script is called directly", os.path.dirname(sys.argv[0]))
-            submodule = Substraction(args.number_1, args.number_2)
+            submodule = Subtraction(args.number_1, args.number_2)
             submodule.run()
         else:
             print("Error printing arguments")
@@ -63,7 +63,7 @@ def main(number_1="", number_2=""):
         args.number_1 = number_1
         args.number_2 = number_2
 
-        submodule = Substraction(args.number_1, args.number_2)
+        submodule = Subtraction(args.number_1, args.number_2)
         submodule.run()
 
 
